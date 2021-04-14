a3b834ffbe3e945634dd2aa0477b5394b12d8606
Natan Trosman <you@example.comG00347770@gmit.ie>
2021-04-14 15:15:19 +0100

    lab complete

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../Services/movies.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  movies: any = [];
+  constructor(private moviesService:MoviesService) {
+}
+
+ngOnInit(){
+  this.moviesService.GetMovieData().subscribe(
+  (data)=>{
+      this.movies = data.Search;
+      console.log(this.movies);
+      }
+    );
+  }
+}
